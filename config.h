6d7faf06c7b4bd58e7c3e9b172161a1999c100b3
theNEOkun <sven@raswill.se>
2022-05-05 07:19:06 +0200

    Added dwm-dragmfact

--- config.h
+++ config.h
@@ -130,6 +130,7 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button1,        moveorplace,    {.i = 2} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,			MODKEY|ShiftMask, Button1,		dragmfact,		{0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
