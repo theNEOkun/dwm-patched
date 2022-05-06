8d86c088f5524c88cd8ec1eb6f34153b217b6712
theNEOkun <sven@raswill.se>
2022-05-06 07:01:58 +0200

    Added a path, and decentralized some shortcuts

--- config.h
+++ config.h
@@ -67,9 +67,9 @@ static const char *powercmd[] = {"rofi-power", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_m,      spawn,          {.v = dmenucmd } },
-	{ Altkey,			            XK_m,      spawn,          {.v = powercmd } },
-	{ MODKEY,						XK_Return, spawn,          {.v = termcmd } },
+	//{ MODKEY,                       XK_m,      spawn,          {.v = dmenucmd } },
+	//{ Altkey,			            XK_m,      spawn,          {.v = powercmd } },
+	//{ MODKEY,						XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstackvis,  {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstackvis,  {.i = -1 } },
