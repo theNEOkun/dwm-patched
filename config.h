3746585d57feb6767ea63667333f1f607862e5f4
theNEOkun <sven@raswill.se>
2022-10-16 12:35:45 +0200

    Changed terminal

--- config.h
+++ config.h
@@ -70,7 +70,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"rofi-run", NULL};//{ "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = {"allacritty", NULL };
 static const char *powercmd[] = {"rofi-power", NULL};
 
 static Key keys[] = {
